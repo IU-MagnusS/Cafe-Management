17f697c0bb851ec947b2becee13cf49f35cedce8
Magnus <magnusnguyen7@gmail.com>
2023-06-23 14:56:42 +0700

    Adjust Bcrypt to encode password

--- com.magnus.cafe/src/main/java/com/magnus/cafe/contents/CafeContents.java
+++ com.magnus.cafe/src/main/java/com/magnus/cafe/contents/CafeContents.java
@@ -5,4 +5,8 @@ public class CafeContents {
     public static final String SOMETHING_WENT_WRONG = "Something went wrong";
 
     public static final String INVALID_DATA = "Invalid Data";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access";
+
+
 }
--- com.magnus.cafe/src/main/java/com/magnus/cafe/restImpI/UserRestImpI.java
+++ com.magnus.cafe/src/main/java/com/magnus/cafe/restImpI/UserRestImpI.java
@@ -4,11 +4,14 @@ import com.magnus.cafe.contents.CafeContents;
 import com.magnus.cafe.rest.UserRest;
 import com.magnus.cafe.service.UserService;
 import com.magnus.cafe.utils.CafeUtils;
+import com.magnus.cafe.wrapper.UserWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -36,4 +39,29 @@ public class UserRestImpI implements UserRest {
         }
         return CafeUtils.getResponseEntity(CafeContents.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @Override
+    public ResponseEntity<List<UserWrapper>> getAllUser() {
+        try {
+            return userService.getAllUser();
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<UserWrapper>>( new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<String> update(Map<String, String> requestMap) {
+        try {
+            return userService.update(requestMap);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeContents.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 }
