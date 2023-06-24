fee0e016d15d2941cd614274bb5154a97fd0acc4
Magnus <magnusnguyen7@gmail.com>
2023-06-24 22:44:48 +0700

    Delete , Update status, Get by id and Get by Category

--- com.magnus.cafe/src/main/java/com/magnus/cafe/dao/ProductDao.java
+++ com.magnus.cafe/src/main/java/com/magnus/cafe/dao/ProductDao.java
@@ -3,11 +3,22 @@ package com.magnus.cafe.dao;
 import com.magnus.cafe.POJO.Product;
 import com.magnus.cafe.wrapper.ProductWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<ProductWrapper> getAllProduct();
+
+    @Modifying
+    @Transactional
+    Integer updateProductStatus(@Param("status") String status, @Param("id") Integer id);
+
+    List<ProductWrapper> getProductByCategory(@Param("id") Integer id);
+
+    ProductWrapper getProductById(@Param("id") Integer id);
 
 }
