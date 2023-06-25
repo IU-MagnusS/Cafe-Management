ab74b6e25420b6bafb41395d4eef0d5338a933d9
Magnus <magnusnguyen7@gmail.com>
2023-06-25 22:02:27 +0700

    Dashboard API to get product, bill, category

---
+++ com.magnus.cafe/src/main/java/com/magnus/cafe/serviceImpI/DashboardServiceImpI.java
@@ -0,0 +1,37 @@
+package com.magnus.cafe.serviceImpI;
+
+import com.magnus.cafe.dao.BillDao;
+import com.magnus.cafe.dao.CategoryDao;
+import com.magnus.cafe.dao.ProductDao;
+import com.magnus.cafe.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class DashboardServiceImpI implements DashboardService {
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    ProductDao productDao;
+
+    @Autowired
+    BillDao billDao;
+
+
+    @Override
+    public ResponseEntity<Map<String, Object>> getCount() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("category", categoryDao.count());
+        map.put("product", productDao.count());
+        map.put("bill", billDao.count());
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+}
